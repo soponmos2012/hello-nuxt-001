3eee5b18fe31de8cf5e8481b142a45fbb07ed388
soponmos <soponmos2012@gmail.com>
2024-01-15 17:29:54 +0700

    edit_playgame

--- components/Playgame.vue
+++ components/Playgame.vue
@@ -1,34 +1,86 @@
 <template>
     <div class="">
         <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css" rel="stylesheet" />
-        <div class="body_content promotion">
-            <div class="content_promotion">
-                <div class="margin-32px large"></div>
-                <div class="margin-16px small"></div>
-
-                <div class="row_promotion_content">
-                    <div class="left">
-                        <img src="https://sale3.pirate168.com/images/promotion_img.png" alt="">
-                        <div class="center_title pentor-bold">PROMOTION</div>
-                    </div>
-                    <div class="right">
-                        <div class="short_content">
-                            <p class="head_pro_title text-yello">ฝากแรก 100 รับ 300</p>
-                            <p class="short_detail">เริ่มตั้งแต่วันนี้ สมาชิกใหม่สามารถรับโบนัสเงินฝากได้หลังจาก
-                                ลงทะเบียนสำเร็จ รับทันที 300 สำหรับฝากแรก</p>
+        <div class="body_content games">
+            <div class="row group_menu content-center">
+                <div class="col-12 col-lg-3 cursor_pointer active">
+                    <img class="large_img" src="/image/allgame.jpg" alt="">
+                </div>
+                <div class="col-6 col-lg-3 cursor_pointer select casino">
+                    <img src="/image/casino.jpg" alt="">
+                </div>
+                <div class="col-6 col-lg-3 cursor_pointer select slot">
+                    <img src="/image/slot.jpg" alt="">
+                </div>
+            </div>
+            <div class="margin-32px large"></div>
+            <div class="margin-16px mobile"></div>
+            <div class="row_content_games">
+                <div class="list_game">
+                    <div class="bg_title_outer casino">
+                        <div class="bg_title_inside">
+                            <div class="text-gold pentor-bold text-center">คาสิโนออนไลน์</div>
                         </div>
-                        <hr>
-                        <div class="group_btn d-flex">
-                            <div class="outside_menu detail_modal" data-key="3">
-                                <div class="inside_menu d-flex">
-                                    <div class="text-gold pentor-bold">ข้อมูลเพิ่มเติม</div>
-                                </div>
-                            </div>
-                            <div class="outside_menu">
-                                <div class="inside_menu d-flex">
-                                    <div class="register text-gold pentor-bold">สมัคร</div>
-                                </div>
-                            </div>
+                    </div>
+                    <div class="margin_casino margin-16px"></div>
+                    <div class="sort_casino">
+                        <div class="cob_ cursor_pointer example">
+                            <img src="https://sale3.pirate168.com/images/listgame/casino_0.png" alt="">
+                        </div>
+                        <div class="cob_ cursor_pointer example">
+                            <img src="https://sale3.pirate168.com/images/listgame/casino_1.png" alt="">
+                        </div>
+                        <div class="cob_ cursor_pointer example">
+                            <img src="https://sale3.pirate168.com/images/listgame/casino_2.png" alt="">
+                        </div>
+                        <div class="cob_ cursor_pointer example">
+                            <img src="https://sale3.pirate168.com/images/listgame/casino_3.png" alt="">
+                        </div>
+                        <div class="cob_ cursor_pointer example">
+                            <img src="https://sale3.pirate168.com/images/listgame/casino_4.png" alt="">
+                        </div>
+                    </div>
+                    <div class="margin-32px large"></div>
+                    <div class="margin-16px mobile"></div>
+                    <div class="bg_title_outer slot">
+                        <div class="bg_title_inside">
+                            <div class="text-gold pentor-bold text-center">สล็อตออนไลน์</div>
+                        </div>
+                    </div>
+                    <div class="margin_slot margin-16px"></div>
+                    <div class="sort_slot">
+                        <div class="cob_ cursor_pointer example">
+                            <img src="https://sale3.pirate168.com/images/listgame/slot_0.png" alt="">
+                        </div>
+                        <div class="cob_ cursor_pointer example">
+                            <img src="https://sale3.pirate168.com/images/listgame/slot_1.png" alt="">
+                        </div>
+                        <div class="cob_ cursor_pointer example">
+                            <img src="https://sale3.pirate168.com/images/listgame/slot_2.png" alt="">
+                        </div>
+                        <div class="cob_ cursor_pointer example">
+                            <img src="https://sale3.pirate168.com/images/listgame/slot_3.png" alt="">
+                        </div>
+                        <div class="cob_ cursor_pointer example">
+                            <img src="https://sale3.pirate168.com/images/listgame/slot_4.png" alt="">
+                        </div>
+                        <div class="cob_ cursor_pointer example">
+                            <img src="https://sale3.pirate168.com/images/listgame/slot_5.png" alt="">
+                        </div>
+                        <div class="cob_ cursor_pointer example">
+                            <img src="https://sale3.pirate168.com/images/listgame/slot_6.png" alt="">
+                        </div>
+                        <div class="cob_ cursor_pointer example">
+                            <img src="https://sale3.pirate168.com/images/listgame/slot_7.png" alt="">
+                        </div>
+                        <div class="cob_ cursor_pointer example">
+                            <img src="https://sale3.pirate168.com/images/listgame/slot_8.png" alt="">
+                        </div>
+                        <div class="cob_ cursor_pointer example">
+                            <img src="https://sale3.pirate168.com/images/listgame/slot_9.png" alt="">
+                        </div>
+                        <div class="cob_ cursor_pointer example">
+                            <img src="https://sale3.pirate168.com/images/listgame/slot_10.png" alt="">
                         </div>
                     </div>
                 </div>
@@ -43,4 +95,61 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style>
+.body_content {
+    padding: 0.5rem 20rem 0rem 20rem;
+}
+.content-center {
+    justify-content: center!important;
+}
+.row {
+    --bs-gutter-x: 1.5rem;
+    --bs-gutter-y: 0;
+    display: flex;
+    flex-wrap: wrap;
+    margin-top: calc(-1 * var(--bs-gutter-y));
+    margin-right: calc(-0.5 * var(--bs-gutter-x));
+    margin-left: calc(-0.5 * var(--bs-gutter-x));
+}
+.active {
+    filter: unset;
+}
+.select {
+    filter: saturate(0);
+}
+.margin-32px {
+    margin-top: 2rem;
+}
+.sort_casino {
+    display: flex;
+    flex-wrap: wrap;
+    margin-right: -0.5rem;
+    margin-left: -0.5rem;
+    margin-top: -1rem;
+}
+div.cob_ {
+    flex: 0 0 auto;
+    max-width: 100%;
+    margin-top: 0.625rem;
+    padding-right: 0.3125rem;
+    padding-left: 0.3125rem;
+    width: 13.3rem;
+}
+img {
+    max-width: 100%;
+}
+.sort_slot {
+    display: flex;
+    flex-wrap: wrap;
+    margin-right: -0.5rem;
+    margin-left: -0.5rem;
+    margin-top: -1rem;
+}
+
+.text-center {
+    background: linear-gradient(90deg, #AE8625 0%, #F7EF8A 36.46%, #D2AC47 79.69%, #EDC967 100%);
+    -webkit-background-clip: text;
+    background-clip: text;
+    font-size: 1.5rem;
+  }
+</style>
